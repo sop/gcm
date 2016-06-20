b97a1f329fb07a9020c2bec819a88b7aa56109cf
Joni Eskelinen <joni@mobie.fi>
2016-06-20 09:57:30 +0300

    Add examples

---
+++ examples/encrypt.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Encrypt a message with additional authenticated data, using a 128-bit key.
+ *
+ * php encrypt.php
+ */
+
+use GCM\AESGCM;
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+// data to encrypt
+$plaintext = "Meet me at the pier at midnight.";
+// additional authenticated data
+// this is only integrity protected but not encrypted
+$aad = "Additional info";
+// encryption key
+$key = "some 128 bit key";
+// random initialization vector
+$iv = openssl_random_pseudo_bytes(12);
+// encrypt and generate the authentication tag
+list($ciphertext, $auth_tag) = AESGCM::encrypt($plaintext, $aad, $key, $iv);
+// print the ciphertext along with the authentication tag
+// and the initialization vector
+echo bin2hex($ciphertext) . "\n" .
+	bin2hex($auth_tag) . "\n" .
+	bin2hex($iv) . "\n";
---
+++ examples/explicit-encrypt.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Encrypt a message without additional authenticated data using AES-192
+ * as an underlying cipher and produce a 104-bit (13 bytes) authentication tag.
+ *
+ * php explicit-encrypt.php
+ */
+
+use GCM\Cipher\AES\AES192Cipher;
+use GCM\GCM;
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+$plaintext = "Secret message.";
+// 192-bit encryption key
+$key = "012345678901234567890123";
+// random 128-bit initialization vector
+$iv = openssl_random_pseudo_bytes(16);
+// configure GCM object with AES-192 cipher and 13-bytes long authentication tag
+$gcm = new GCM(new AES192Cipher(), 13);
+// encrypt and generate authentication tag
+list($ciphertext, $auth_tag) = $gcm->encrypt($plaintext, "", $key, $iv);
+// print the ciphertext along with the authentication tag
+// and the initialization vector
+echo bin2hex($ciphertext) . "\n" .
+	bin2hex($auth_tag) . "\n" .
+	bin2hex($iv) . "\n";
